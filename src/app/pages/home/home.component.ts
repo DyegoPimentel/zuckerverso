a00d1a127227d035e6485d0431cd11d74dc58a1f
Dyego Pimentel <dyegobusiness@gmail.com>
2024-06-11 23:48:45 -0400

    angular material install

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
