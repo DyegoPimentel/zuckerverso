ad5cba88676a89c49d4a32d0cabbba6275c9aea3
Dyego Pimentel <dyegobusiness@gmail.com>
2024-06-11 23:10:05 -0400

    rota inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Histórias do Brasil',
+        loadComponent: () => import('./pages/home/home.component'),
+    }
+];
