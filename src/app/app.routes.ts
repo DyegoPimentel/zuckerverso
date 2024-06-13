ee98ca5d36dd3daf5ec3083315e7964650f4e12f
Dyego Pimentel <dyegobusiness@gmail.com>
2024-06-12 23:49:07 -0400

    update routes and config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,10 @@ export const routes: Routes = [
         path: '',
         title: 'Histórias do Brasil',
         loadComponent: () => import('./pages/home/home.component'),
+    },
+    {
+        path: 'collection/list',
+        title: 'Lista de NFT - Histórias do Brasil',
+        loadComponent: () => import('./pages/collection/list/list.component')
     }
 ];
