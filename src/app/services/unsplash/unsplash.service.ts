9abb239520842d7a42d08d03ff9a40dd567d4920
Dyego Pimentel <dyegobusiness@gmail.com>
2024-06-13 23:26:01 -0400

    fix: unsplash service

--- src/app/services/unsplash/unsplash.service.ts
+++ src/app/services/unsplash/unsplash.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class UnsplashService {
   private apiUrl = 'https://api.unsplash.com';
-  private accessKey = environment.unsplashAccessKey;
+  private API_KEY = environment.UNSPLASH_API_KEY;
 
   constructor(private http: HttpClient) { 
 
@@ -16,7 +16,7 @@ export class UnsplashService {
 
   getRandomPhotos(query: string, count: number = 1): Observable<any> {
     const headers = new HttpHeaders({
-      'Authorization': `Client-ID ${this.accessKey}`
+      'Authorization': `Client-ID ${this.API_KEY}`
     });
     return this.http.get(`${this.apiUrl}/photos/random?query=${query}&count=${count}`, { headers });
   }
