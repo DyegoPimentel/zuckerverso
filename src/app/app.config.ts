9b3541d0e156f09920bd9d73bc322ed2ec519235
Dyego Pimentel <dyegobusiness@gmail.com>
2024-06-13 23:24:28 -0400

    chore: set httpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { ActivatedRoute, provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MetamaskService } from './services/authentication/metamask.service';
+import { provideHttpClient } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes, withComponentInputBinding()), 
     provideAnimationsAsync(),
-    MetamaskService
+    provideHttpClient(),
+    MetamaskService,
   ]
 };
