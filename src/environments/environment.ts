d8c89596601ed66c8a983a35769fc53a97d67723
Dyego Pimentel <dyegobusiness@gmail.com>
2024-06-17 12:02:55 -0400

    update

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    OPENSEA_API_KEY: '6b430991dca241ce9a4a3a5148734f93',
+    firebase: {
+        apiKey: "AIzaSyAEBNhgtViPSqZQRmFAoBlL3lGlfja3-xM",
+        authDomain: "zuckerverso.firebaseapp.com",
+        projectId: "zuckerverso",
+        storageBucket: "zuckerverso.appspot.com",
+        messagingSenderId: "263267910515",
+        appId: "1:263267910515:web:886c3ecba71d05d5f43b06"
+    }
+};
