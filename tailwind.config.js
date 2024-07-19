64498cdb52dd0bf610497a66393371f86fbc9c34
Dyego Pimentel <dyegobusiness@gmail.com>
2024-07-19 19:48:45 -0400

    fix: primary collor ajust

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#3C095E',
+        primary: {
+          DEFAULT: '#3C095E',
+          100: '#E8E0EF',  // Lighter shade
+          200: '#C5B2DB',
+          300: '#A384C8',
+          400: '#8056B4',
+          500: '#3C095E',  // Base color
+          600: '#350850',
+          700: '#2E0743',
+          800: '#270536',
+          900: '#200429',  // Darker shade
+        },
       },
     },
   },
